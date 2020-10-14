7867c55f4af7c3a8afd4f605b8930ac0e6f9ba29
houtan <houtan@autohome.com.cn>
2020-10-14 19:29:27 +0800

    [update]我的决定列表页增删改功能

--- app.js
+++ app.js
@@ -35,6 +35,106 @@ App({
   },
   globalData: {
     userInfo: null,
-    currentItem: {} // 编辑决定的JOSN
+    myDecisionList: [ // 我的决定列表数据
+      {
+        id: 1,
+        title: '怎么样原谅男盆友？',
+        list: [
+          {
+            id: 11,
+            value: '写1000个我爱你',
+            weight: '10%'
+          },
+          {
+            id: 12,
+            value: '鬼搓衣板',
+            weight: '10%'
+          },
+          {
+            id: 13,
+            value: '海底捞',
+            weight: '10%'
+          },
+          {
+            id: 14,
+            value: '无条件原谅',
+            weight: '10%'
+          }
+        ]
+      },
+      {
+        id: 2,
+        title: '谁来做家务',
+        list: [
+          {
+            id: 21,
+            value: '老公',
+            weight: '10%'
+          },
+          {
+            id: 22,
+            value: '老婆',
+            weight: '10%'
+          }
+        ]
+      },
+      {
+        id: 3,
+        title: '掷骰子',
+        list: [
+          {
+            id: 31,
+            value: 1,
+            weight: '10%'
+          },
+          {
+            id: 32,
+            value: 2,
+            weight: '10%'
+          },
+          {
+            id: 33,
+            value: 3,
+            weight: '10%'
+          },
+          {
+            id: 34,
+            value: 4,
+            weight: '10%'
+          },
+          {
+            id: 35,
+            value: 5,
+            weight: '10%'
+          },
+          {
+            id: 36,
+            value: 6,
+            weight: '10%'
+          }
+        ]
+      },
+      {
+        id: 4,
+        title: '你是什么垃圾？',
+        list: [
+          {
+            id: 41,
+            value: '可回收垃圾',
+            weight: '10%'
+          },
+          {
+            id: 42,
+            value: '干垃圾',
+            weight: '10%'
+          },
+          {
+            id: 43,
+            value: '有害垃圾',
+            weight: '10%'
+          }
+        ]
+      }
+    ]
   }
 })
--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -4,11 +4,62 @@ const app = getApp()
 
 Page({
   data: {
-    myDecision: {}
+    myDecision: {},
+    currentIndex: ''
   },
-  onReady() {
+  onLoad(options) {
+    let currentIndex = app.globalData.myDecisionList.findIndex((val, key) => {
+      return val.id === Number(options.id);
+    });
     this.setData({
-      myDecision: app.globalData.currentItem
+      myDecision: JSON.parse(JSON.stringify(app.globalData.myDecisionList[currentIndex])),
+      currentIndex: options.index
+    });
+  },
+  titleInputHandel: function (e) {
+    console.log(e.detail.value);
+    this.setData({
+      [`myDecision.title`]:  e.detail.value
+    });
+  },
+  // 问题列表修改
+  contentInputHandel: function (e) {
+    // console.log(e);
+    this.setData({
+      [`myDecision.list[${e.target.dataset.index}].value`]: e.detail.value
+    }, function () {
+      console.log(this.data.myDecision);
+    })
+  },
+  // 删除
+  deleteHandel: function (e) {
+    console.log(e.target.dataset.index)
+    this.data.myDecision.list.splice(e.target.dataset.index, 1);
+    this.setData({
+      [`myDecision.list`]: this.data.myDecision.list
+    });
+  },
+  // 新增
+  addNewHandel: function () {
+    let obj = {
+      id: Number(`${this.data.currentIndex}${this.data.myDecision.list.length + 1}`),
+      value: '',
+      weight: '10%'
+    }
+    this.data.myDecision.list.push(obj)
+    this.setData({
+      myDecision: this.data.myDecision
+    })
+  },
+  confirm: function() {
+    console.log(this.data);
+    let that = this;
+    let currentIndex = app.globalData.myDecisionList.findIndex((val, key) => {
+      return val.id === that.data.myDecision.id;
+    });
+    app.globalData.myDecisionList[currentIndex] = that.data.myDecision;
+    wx.navigateBack({
+      delta: 1
     });
   }
 })
